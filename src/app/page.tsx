c7a39c9aba2231fe37866a4a12aa5859f33e7f32
Krisdr00m <kristophe.yen1@gmail.com>
2025-07-19 23:27:37 -0400

    placeholder for main page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p className="text-lg">This is a simple Next.js app with Tailwind CSS.</p>
+      <Image
+        src="/next.svg"
+        alt="Next.js logo"
+        width={180}
+        height={38}
+        priority
+      />
+    </div>
     // <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
     //   <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
     //     <Image
