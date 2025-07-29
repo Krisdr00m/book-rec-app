d17991dc49e2fbc3d64dd30a12f3c75d432e9517
Krisdr00m <kristophe.yen1@gmail.com>
2025-07-29 00:02:40 -0400

    just added some potentential data fetchers

--- src/app/lib/api/book-data.ts
+++ src/app/lib/api/book-data.ts
@@ -1,7 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 
+// This is the solr query so you can free type in here
+export async function getSearchedBook(freeSearch: string){
+    const res = await fetch('https://openlibrary.org/search.json?q=' + freeSearch);
+    return res.json();
+}
 
-export async function getSearchedBook(title: string){
-    const res = await fetch('https://openlibrary.org/search.json?q=' + title);
+export async function getBookByAuthor(authorName:string) {
+    const res = await fetch('https://openlibrary.org/search.json?author=' + authorName);
+    return res.json();
+}
+
+export async function getBookByTitle(bookTitle:string) {
+    const res = await fetch('https://openlibrary.org/search.json?title=' +  bookTitle);
     return res.json();
 }
