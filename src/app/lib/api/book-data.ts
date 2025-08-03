9fd2e4a19ab7dfe4e9ee9c4b43ca3da80b382349
Krisdr00m <kristophe.yen1@gmail.com>
2025-08-03 15:51:42 -0400

    add some error handling on api call

--- src/app/lib/api/book-data.ts
+++ src/app/lib/api/book-data.ts
@@ -3,15 +3,32 @@ import { NextRequest, NextResponse } from "next/server";
 // This is the solr query so you can free type in here
 export async function getSearchedBook(freeSearch: string){
     const res = await fetch('https://openlibrary.org/search.json?q=' + freeSearch);
+    if (!res.ok) {
+        throw new Error(`Failed to fetch search results: ${res.statusText}`);
+    }
     return res.json();
 }
 
 export async function getBookByAuthor(authorName:string) {
     const res = await fetch('https://openlibrary.org/search.json?author=' + authorName);
+    if (!res.ok) {
+        throw new Error(`Failed to fetch book by author: ${res.statusText}`);
+    }
     return res.json();
 }
 
 export async function getBookByTitle(bookTitle:string) {
     const res = await fetch('https://openlibrary.org/search.json?title=' +  bookTitle);
+    if (!res.ok) {
+        throw new Error(`Failed to fetch book by title: ${res.statusText}`);
+    }
     return res.json();
+}
+
+export async function getCoverByID(coverID: string){
+    const res = await fetch('https://covers.openlibrary.org/b/id/' + coverID + '-L.jpg');
+
+    if (!res.ok) {
+        throw new Error(`Failed to fetch cover image: ${res.statusText}`);
+    }
 }
