ae116b7c1d7107b1a321da81b9952e57886ef01a
Krisdr00m <kristophe.yen1@gmail.com>
2025-08-05 22:31:52 -0400

    added new component and tested

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
 import SearchComponent from "./components/search-component";
+import MediaCard from "./components/book-card";
 export default function Home() {
   return (
     <div>
       <SearchComponent />
+      <div style={{ display: "flex", flexWrap: "wrap", gap: "16px" }}>
+        <MediaCard />
+        <MediaCard />
+        <MediaCard />
+        <MediaCard />
+        <MediaCard />
+        <MediaCard />
+      </div>
     </div>
     // <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
     //   <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
