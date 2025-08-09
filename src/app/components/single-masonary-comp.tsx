2c32fe015ce452459dd6a90aba620b50a88f471e
Krisdr00m <kristophe.yen1@gmail.com>
2025-08-09 14:07:25 -0400

    change css on masonary single comp

--- src/app/components/single-masonary-comp.tsx
+++ src/app/components/single-masonary-comp.tsx
@@ -21,9 +21,12 @@ export default function BookMasonarySingle({book = tempBook}: BookMasonarySingle
     return (
         <div style ={{minHeight: '200px'}}className="book-masonary-single">
             <a href={book.indivLink} className="book-link">
-                <Paper sx={{minHeight: '200px'}} square = {false} elevation={20} className="book-paper">
-                    <img src={book.coverImage} alt={book.title} className="book-cover" />
-                    <h3 className="book-title">{book.title}</h3>  
+                <Paper sx={{minHeight: '200px', minWidth: '150px', maxWidth: '200px'}} square = {false} elevation={20} className="book-paper">
+                     <img style={{height:'90%', width: '100%'}} src={book.coverImage} alt={book.title} className="book-cover" />
+                    <h3 style = {{height: '10%'}}className="book-title">{book.title}</h3>  
+                
+                    {/* <img style={{height:'90%'}} src={book.coverImage} alt={book.title} className="book-cover" />
+                    <h3 style = {{height: '10%'}}className="book-title">{book.title}</h3>   */}
                 </Paper>
 
             </a>
