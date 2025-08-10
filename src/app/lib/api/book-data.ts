34cba3c4c08bf3b183d11757e9772c0c09f2de64
Krisdr00m <kristophe.yen1@gmail.com>
2025-08-10 15:00:22 -0400

    updated routes with backticks and added new function

--- src/app/lib/api/book-data.ts
+++ src/app/lib/api/book-data.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 // This is the solr query so you can free type in here
 export async function getSearchedBook(freeSearch: string){
-    const res = await fetch('https://openlibrary.org/search.json?q=' + freeSearch);
+    const res = await fetch(`https://openlibrary.org/search.json?q=${freeSearch}`);
     if (!res.ok) {
         throw new Error(`Failed to fetch search results: ${res.statusText}`);
     }
@@ -10,7 +10,7 @@ export async function getSearchedBook(freeSearch: string){
 }
 
 export async function getBookByAuthor(authorName:string) {
-    const res = await fetch('https://openlibrary.org/search.json?author=' + authorName);
+    const res = await fetch(`https://openlibrary.org/search.json?author=${authorName}`);
     if (!res.ok) {
         throw new Error(`Failed to fetch book by author: ${res.statusText}`);
     }
@@ -18,15 +18,15 @@ export async function getBookByAuthor(authorName:string) {
 }
 
 export async function getBookByTitle(bookTitle:string) {
-    const res = await fetch('https://openlibrary.org/search.json?title=' +  bookTitle);
+    const res = await fetch(`https://openlibrary.org/search.json?title=${bookTitle}`);
     if (!res.ok) {
         throw new Error(`Failed to fetch book by title: ${res.statusText}`);
     }
     return res.json();
 }
 
-export async function getCoverByID(coverID: string){
-    const res = await fetch('https://covers.openlibrary.org/b/id/' + coverID + '-L.jpg');
+export async function getCoverByID(coverID: string, size: string) {
+    const res = await fetch(`https://covers.openlibrary.org/b/id/${coverID}-${size}.jpg`);
 
     if (!res.ok) {
         throw new Error(`Failed to fetch cover image: ${res.statusText}`);
