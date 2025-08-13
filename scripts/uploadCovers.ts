27be7778a713530b3d009ccc10b6d755e8dc5172
Krisdr00m <kristophe.yen1@gmail.com>
2025-08-12 21:20:33 -0400

    updating uploadCovers script

--- scripts/uploadCovers.ts
+++ scripts/uploadCovers.ts
@@ -1,11 +1,11 @@
 require('dotenv').config({ path: '.env' });
 import { createSupaClient } from "../src/app/lib/api/supabase";
-import {getCoverByID} from '../src/app/lib/api/book-data';
+// import {getCoverByID} from '../src/app/lib/api/book-data';
 import * as fs from 'fs';
 
 const supabase = createSupaClient();
-const filePath = '';
-
+const filePath = 'C:\Users\yenkr\OneDrive\Desktop\Coding-Shii\Python-Scripts\book-data.json';
+const basePath = 'C:\Users\yenkr\OneDrive\Desktop\Coding-Shii\Python-Scripts\book_covers'
 async function read_File(): Promise<any[]> {
     try {
         const data = await fs.promises.readFile(filePath, 'utf8');
@@ -25,9 +25,13 @@ export async function uploadCovers(){
         for (const book of data){
             try{
                 let cover_i = book.cover_i
+                let bookTitle = book.title
                 if(cover_i){
-                    const coverImageSmall = await getCoverByID(cover_i.toString(), 'S');
-                    const coverImageMedium = await getCoverByID(cover_i.toString(), 'M');
+                    console.log(`Uploading cover for ${book.title} by ${book.author_name[0]}`);
+                    let path 
+                    // const {data , error} = await supabase.storage.from('Covers').upload(`${basePath}/${cover
+                    // const coverImageSmall = await getCoverByID(cover_i.toString(), 'S');
+                    // const coverImageMedium = await getCoverByID(cover_i.toString(), 'M');
                 }
             }catch (error){
                 console.error('Error uploading cover:', error);
